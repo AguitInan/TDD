ab2db068a91ae6a683641d2e01d1875380b9e1bf
unknown <aguit.inan@gmail.com>
2024-01-25 14:47:12 +0100

    GREEN 1

--- Exercice03.Core/Class1.cs
+++ Exercice03.Core/Class1.cs
@@ -1,7 +1,34 @@
-﻿namespace Exercice03.Core
-{
-    public class Class1
-    {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
+namespace Exercice03.Core
+{
+    public class RechercheVille
+    {
+        private List<string> _villes;
+
+        public RechercheVille()
+        {
+            _villes = new List<string> {
+            "Paris", "Budapest", "Skopje", "Rotterdam", "Valence",
+            "Vancouver", "Amsterdam", "Vienne", "Sydney", "New York",
+            "Londres", "Bangkok", "Hong Kong", "Dubaï", "Rome", "Istanbul"
+            };
+        }
+
+        public List<string> Rechercher(string mot)
+        {
+            if (mot.Length < 2)
+            {
+                throw new NotFoundException();
+            }
+            return _villes.Where(ville => ville.StartsWith(mot, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
     }
+
+    public class NotFoundException : Exception
+    {
+    }
+
 }
